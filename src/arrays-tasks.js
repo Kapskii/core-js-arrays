743971b6aa2c54bcb49cc2951f7b6500c84b6090
Kapskii <kapskij20@gmail.com>
2024-01-03 14:57:17 +0300

    add 3 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -98,8 +98,16 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter(
+    (el) =>
+      el !== false &&
+      el !== null &&
+      el !== 0 &&
+      el !== '' &&
+      el !== undefined &&
+      !Number.isNaN(el)
+  );
 }
 
 /**
@@ -112,8 +120,8 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((el) => el.length);
 }
 
 /**
